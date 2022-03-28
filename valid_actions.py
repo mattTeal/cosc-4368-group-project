883b90b4a20094e6b6e13132527afd6bcf751dc9
Matthew Teal <89936190+tattmeal@users.noreply.github.com>
2022-03-27 22:01:22 -0500

    removed whitespace in validactions

---
+++ valid_actions.py
@@ -0,0 +1,70 @@
+import main
+
+def aplop(i, j, i2, j2, x, x2, a, b, c, d, e, f):
+  fValidMoves = set()
+  mValidMoves = set()
+  # Female agent actions
+  if (main.world[i][j] == 1):  # and !female.isHoldingBlock
+    fValidMoves.add('D')
+  elif (main.world[i][j] == 2):  # and female.isHoldingBlock
+    fValidMoves.add('P')
+  else: 
+    if (i == 0):
+      fValidMoves.add('S')
+      if (j == 0):
+        fValidMoves.add('E')
+      elif (j == 4):
+        fValidMoves.add('W')
+    elif (i == 4):
+      fValidMoves.add('N')
+      if (j == 0):
+        fValidMoves.add('E')
+      elif (j == 4):
+        fValidMoves.add('W')
+      else:
+        fValidMoves.add('W')
+        fValidMoves.add('E')
+    else:
+      fValidMoves.add('N')
+      fValidMoves.add('S')
+      if (j == 0):
+        fValidMoves.add('E')
+      elif (j == 4):
+        fValidMoves.add('W')
+      else:
+        fValidMoves.add('W')
+        fValidMoves.add('E')
+
+  # Male agent actions
+  if (main.world[i2][j2] == 1):  # and !Male.isHoldingBlock
+    mValidMoves.add('D')
+  elif (main.world[i2][j2] == 2):  # and Male.isHoldingBlock
+    mValidMoves.add('P')
+  else: 
+    if (i2 == 0):
+      mValidMoves.add('S')
+      if (j2 == 0):
+        mValidMoves.add('E')
+      elif (j2 == 4):
+        mValidMoves.add('W')
+    elif (i2 == 4):
+      mValidMoves.add('N')
+      if (j2 == 0):
+        mValidMoves.add('E')
+      elif (j2 == 4):
+        mValidMoves.add('W')
+      else:
+        mValidMoves.add('W')
+        mValidMoves.add('E')
+    else:
+      mValidMoves.add('N')
+      mValidMoves.add('S')
+      if (j2 == 0):
+        mValidMoves.add('E')
+      elif (j2 == 4):
+        mValidMoves.add('W')
+      else:
+        mValidMoves.add('W')
+        mValidMoves.add('E')
+
+  return fValidMoves, mValidMoves 
