51e47d25237fc3817c3887c836f31b93e8391e02
Matthew Teal <89936190+tattmeal@users.noreply.github.com>
2022-03-27 20:00:56 -0500

    initializing world.

---
+++ pd_world.py
@@ -0,0 +1,15 @@
+# Dropoff = 1
+# Pickup = 2
+# Neutral = 0
+
+world = [
+    [1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 2],
+    [0, 2, 0, 0, 0],
+    [0, 0, 0, 0, 1]
+]
+
+# Starting locations
+male_agent_location = [4, 2]
+female_agent_location = [0, 2]
